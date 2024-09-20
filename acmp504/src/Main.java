85be856da7816c9efd185d401780c380ff6589b4
balteyes <julian.ivanova@gmail.com>
2024-09-20 14:03:55 +0300

    добавлен метод swap для обмена элементов внутри массива

--- acmp504/src/Main.java
+++ acmp504/src/Main.java
@@ -6,18 +6,18 @@ public class Main {
         char geran ='G';
         char krokus = 'C';
         char fialka = 'V';
-        ;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите количество дней");
         int K = scanner.nextInt();
         char[] flowers ={'G','C','V'};
 
         for (int i = 0; i < K; i++) {
-
-
+            swap(flowers, 1, 2);
+            swap(flowers, 0, 1);
 
         }
-
+        System.out.println(new String(flowers));
 
         }
     public static void swap(char[]mas, int i, int j){
