6c87d85803f27480f6495f83327061b3d0ad6558
balteyes <julian.ivanova@gmail.com>
2024-09-20 12:45:19 +0300

    функция

---
+++ acmp504/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        char geran ='G';
+        char krokus = 'C';
+        char fialka = 'V';
+        ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите количество дней");
+        int K = scanner.nextInt();
+        char[] flowers ={'G','C','V'};
+
+        for (int i = 0; i < K; i++) {
+
+
+
+        }
+
+
+        }
+    public static void swap(char[]mas, int i, int j){
+        char move = mas[i];
+        mas[i] = mas[j];
+        mas[j] = move;
+
+
+    }
+    }
